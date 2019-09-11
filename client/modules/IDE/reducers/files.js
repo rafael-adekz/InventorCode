bb672ee214f9b1ee0d19bcb5b8d34b7b2b120144
Inventor <leonardof13@gmail.com>
2019-09-11 10:13:45 -0300

    fix easyEditor

--- client/modules/IDE/reducers/files.js
+++ client/modules/IDE/reducers/files.js
@@ -16,7 +16,7 @@ const defaultHTML =
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.sound.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/molleindustria/p5.play@master/lib/p5.play.js"></script>
-    <script src="https://rawcdn.githack.com/leonardof131/easyEditor/2cc021929d789d921079be0363dc5d91022b8339/easyEditor.js"></script>
+    <script src="https://rawcdn.githack.com/leonardof131/easyEditor/a942d2c11ad7cf20da5760cd97d103c0b7a040a8/easyEditor.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8" />
 
