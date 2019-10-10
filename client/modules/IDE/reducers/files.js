9e9b49e67ca3dc18501608f7073090c591c0dc01
Inventor <leonardof13@gmail.com>
2019-10-10 16:33:52 -0300

    adicionando bola

--- client/modules/IDE/reducers/files.js
+++ client/modules/IDE/reducers/files.js
@@ -22,7 +22,7 @@ const defaultHTML =
     <script src="https://rawcdn.githack.com/molleindustria/p5.play/42cd19c39f6f508e4a73c20eaaeb490b97022840/lib/p5.play.js"></script>
     <script src="https://rawcdn.githack.com/fabiojavaroni/lib/6138e4caa2446d9294710c1332a663638e85dd02/easyEditor.js"></script>
     <script src="https://rawcdn.githack.com/fabiojavaroni/lib/6138e4caa2446d9294710c1332a663638e85dd02/fundo_VS_3_Com Sprites.js"></script>
-    <script src="https://rawcdn.githack.com/fabiojavaroni/lib/6138e4caa2446d9294710c1332a663638e85dd02/Sprites_VS_3.js"></script>
+    <script src="https://rawcdn.githack.com/fabiojavaroni/lib/ee5c608995a691e43b6358e9feb3d49cc3fc5df7/Sprites_VS_3.js"></script>
     <script src="https://rawcdn.githack.com/yining1023/p5PlayGround/cad807762776d1f29820bc9f35f36e98cc18d934/js/jquery.js" type="text/javascript"> </script>
     <script src="https://rawcdn.githack.com/leonardof131/mousePosP5js/280d22c78e510f1677fb246c62bd7ed755bbe0db/jquery.ruler.js"></script>
     <script>$(function() {$('body').ruler({vRuleSize: 0,hRuleSize: 0,showCrosshair : false,showMousePos: true});});</script>
