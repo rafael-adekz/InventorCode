04df17f2617aab790cc1290a22190eff0df3546f
Inventor <leonardof13@gmail.com>
2019-10-24 10:50:37 -0300

    atualizando libs

--- client/modules/IDE/reducers/files.js
+++ client/modules/IDE/reducers/files.js
@@ -21,9 +21,10 @@ const defaultHTML =
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.sound.min.js"></script>
     <script src="https://rawcdn.githack.com/fabiojavaroni/lib/fabbe712f5eab584006b79e76b50f8679b77e494/Preload.js"></script>
     <script src="https://rawcdn.githack.com/molleindustria/p5.play/42cd19c39f6f508e4a73c20eaaeb490b97022840/lib/p5.play.js"></script>
-    <script src="https://rawcdn.githack.com/fabiojavaroni/lib/e4d430af566bd9f113cb435653eb5560abff848e/easyEditor.js"></script>
+    <script src="https://rawcdn.githack.com/fabiojavaroni/lib/ae92a9c5af76d4d49377ef5938370028b2f62e40/easyEditor.js"></script>
     <script src="https://rawcdn.githack.com/fabiojavaroni/lib/e4d430af566bd9f113cb435653eb5560abff848e/fundo_VS_3_Com Sprites.js"></script>
     <script src="https://rawcdn.githack.com/fabiojavaroni/lib/e4d430af566bd9f113cb435653eb5560abff848e/Sprites_VS_3.js"></script>
+    <script src="https://rawcdn.githack.com/IDMNYU/p5.js-speech/e7ae007d61f048fc2379971b0de7d5db8abb7eee/lib/p5.speech.js"></script>
     <script src="https://rawcdn.githack.com/yining1023/p5PlayGround/cad807762776d1f29820bc9f35f36e98cc18d934/js/jquery.js" type="text/javascript"> </script>
     <script src="https://rawcdn.githack.com/leonardof131/mousePosP5js/280d22c78e510f1677fb246c62bd7ed755bbe0db/jquery.ruler.js"></script>
     <script>$(function() {$('body').ruler({vRuleSize: 0,hRuleSize: 0,showCrosshair : false,showMousePos: true});});</script>
