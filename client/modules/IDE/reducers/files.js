12b3c35c76734a386e5bb796f70518058d0569e1
Inventor <leonardof13@gmail.com>
2019-11-06 13:59:50 -0300

    atualizando libs

--- client/modules/IDE/reducers/files.js
+++ client/modules/IDE/reducers/files.js
@@ -23,7 +23,7 @@ const defaultHTML =
     <script src="https://rawcdn.githack.com/molleindustria/p5.play/42cd19c39f6f508e4a73c20eaaeb490b97022840/lib/p5.play.js"></script>
     <script src="https://rawcdn.githack.com/fabiojavaroni/lib/47aaaa861299830420a36e18fc8a37cb0e43581c/easyEditor.js"></script>
     <script src="https://rawcdn.githack.com/fabiojavaroni/lib/e4d430af566bd9f113cb435653eb5560abff848e/fundo_VS_3_Com Sprites.js"></script>
-    <script src="https://rawcdn.githack.com/fabiojavaroni/lib/7c4d1d7cf7d9bd2887fcdcd46725700e845334f1/Sprites_VS_3.js"></script>
+    <script src="https://rawcdn.githack.com/fabiojavaroni/lib/adf6503246ca6cc9b54cb1760209fac3b0bba397/Sprites_VS_3.js"></script>
     <script src="https://rawcdn.githack.com/IDMNYU/p5.js-speech/e7ae007d61f048fc2379971b0de7d5db8abb7eee/lib/p5.speech.js"></script>
     <script src="https://rawcdn.githack.com/yining1023/p5PlayGround/cad807762776d1f29820bc9f35f36e98cc18d934/js/jquery.js" type="text/javascript"> </script>
     <script src="https://rawcdn.githack.com/leonardof131/mousePosP5js/280d22c78e510f1677fb246c62bd7ed755bbe0db/jquery.ruler.js"></script>
