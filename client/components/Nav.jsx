aaf6138286da99e49f9ed744fa9bc163cac7c9ba
leonardof131 <leonardof13@gmail.com>
2020-03-13 11:07:51 -0300

    atualizando menu biblioteca, ver diff para mais informações

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -738,37 +738,3 @@ const mapDispatchToProps = {
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Nav));
 export { Nav as NavComponent };
-
-// List the photo albums that exist in the bucket.
-function listAlbums() {
-  s3.listObjects({Delimiter: '/'}, function(err, data) {
-    if (err) {
-      return alert('There was an error listing your albums: ' + err.message);
-    } else {
-      var albums = data.CommonPrefixes.map(function(commonPrefix) {
-        var prefix = commonPrefix.Prefix;
-        var albumName = decodeURIComponent(prefix.replace('/', ''));
-        return getHtml([
-          '<li>',
-            '<button style="margin:5px;" onclick="viewAlbum(\'' + albumName + '\')">',
-              albumName,
-            '</button>',
-          '</li>'
-        ]);
-      });
-      var message = albums.length ?
-        getHtml([
-          '<p>Click on an album name to view it.</p>',
-        ]) :
-        '<p>You do not have any albums. Please Create album.';
-      var htmlTemplate = [
-        '<h2>Albums</h2>',
-        message,
-        '<ul>',
-          getHtml(albums),
-        '</ul>',
-      ]
-      document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
-    }
-  });
-}
