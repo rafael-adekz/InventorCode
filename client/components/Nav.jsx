68276b645f9c981780612b98f198af467454eaed
leonardof131 <leonardof13@gmail.com>
2020-03-17 15:57:44 -0300

    testando icones

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -547,7 +547,7 @@ class Nav extends React.PureComponent {
               }}
             >
               <span className="nav__item-header"></span>
-              <InlineSVG className="nav__item-header-triangle" src={shareURL} />
+              <InlineSVG className="nav__item-header-triangle" src={triangleUrl} />
             </button>
             <ul className="nav__dropdown">
               <li className="nav__dropdown-item">
