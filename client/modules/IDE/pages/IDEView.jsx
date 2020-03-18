196207acb15dc0fbb0d472aa0baad2a37ae216c0
leonardof131 <leonardof13@gmail.com>
2020-03-18 14:52:11 -0300

    aidicionando personagens aos routes

--- client/modules/IDE/pages/IDEView.jsx
+++ client/modules/IDE/pages/IDEView.jsx
@@ -34,6 +34,7 @@ import Searchbar from '../components/Searchbar';
 import AssetList from '../components/AssetList';
 import About from '../components/About';
 import Feedback from '../components/Feedback';
+import personagens from '../components/Personagens';
 
 class IDEView extends React.Component {
   constructor(props) {
