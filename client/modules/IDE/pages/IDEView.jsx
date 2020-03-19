52c5c42a9d5dc354381a1f65335cd5c7282f46c6
leonardof131 <leonardof13@gmail.com>
2020-03-19 13:24:04 -0300

    modificando IDEView

--- client/modules/IDE/pages/IDEView.jsx
+++ client/modules/IDE/pages/IDEView.jsx
@@ -35,7 +35,7 @@ import AssetList from '../components/AssetList';
 import About from '../components/About';
 import Feedback from '../components/Feedback';
 //import Personagens from '../components/Personagens';
-//import Personagens from '../components/Personagens';
+import Personagens from '../components/Personagens';
 
 class IDEView extends React.Component {
   constructor(props) {
