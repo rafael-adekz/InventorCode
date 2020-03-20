02ef70c8fb3068e6cbd4fb28608eab6a9789000e
leonardof131 <leonardof13@gmail.com>
2020-03-20 14:28:25 -0300

    teste about2

--- client/modules/IDE/components/About2.jsx
+++ client/modules/IDE/components/About2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import InlineSVG from 'react-inlinesvg';
 import { Helmet } from 'react-helmet';
 
-const squareLogoUrl = require('../../../images/p5js-square-logo.svg');
+const elementosUrl = require('../../../images/elementosicon.svg');
 // const playUrl = require('../../../images/play.svg');
 const asteriskUrl = require('../../../images/p5-asterisk.svg');
 
@@ -13,8 +13,8 @@ function About2(props) {
       <Helmet>
         <title>Editor da Escola de Inventor About</title>
       </Helmet>
-      <div className="about__content-column">
-        <InlineSVG className="about__logo" src={squareLogoUrl} alt="p5js Square Logo" />
+      <div>
+        <InlineSVG className="about__logo" src={elementosUrl} alt="p5js Square Logo" />
         {/* Video button to hello p5 video page */}
         {/* <p className="about__play-video">
           <a
@@ -25,98 +25,6 @@ function About2(props) {
             <InlineSVG className="about__play-video-button" src={playUrl} alt="Play Hello Video" />
           Play hello! video</a>
         </p>  */}
-      </div>
-      <div className="about__content-column">
-        <h3 className="about__content-column-title">Este Site</h3>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          SE VOCE ESTA VENDO ESSA FRASE É PQ CHAMAR A PAGINA FUNCIONOU
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          O que é Este Site?
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          O que é Maker?
-          </a>
-        </p>
-      </div>
-      <div className="about__content-column">
-        <h3 className="about__content-column-title">Recursos</h3>
-        <p className="about__content-column-list">
-          <a
-            href="https://tableless.github.io/iniciantes/manual/js/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          Exemplos
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          Aprenda
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://discourse.processing.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          Comandos
-          </a>
-        </p>
-      </div>
-      <div className="about__footer">
-        <p className="about__footer-list">
-          <a
-            href="https://escoladeinventor.com.br/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Escola de Inventor
-          </a>
-        </p>
-        <p className="about__footer-list">
-          <a
-            href="https://www.facebook.com/escoladeinventor/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Facebook
-          </a>
-        </p>
-        <p className="about__footer-list">
-          <a
-            href="https://www.instagram.com/escoladeinventor/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Instagram
-          </a>
-        </p>
       </div>
     </div>
   );
