b498540ae598a4cdd6fad542398b585ee7a517a1
leonardof131 <leonardof13@gmail.com>
2020-03-20 13:18:40 -0300

    testando mostrador de imagens em about2

--- client/modules/IDE/components/About2.jsx
+++ client/modules/IDE/components/About2.jsx
@@ -1,125 +1,24 @@
-import React from 'react';
-import InlineSVG from 'react-inlinesvg';
-import { Helmet } from 'react-helmet';
-
-const squareLogoUrl = require('../../../images/p5js-square-logo.svg');
-// const playUrl = require('../../../images/play.svg');
-const asteriskUrl = require('../../../images/p5-asterisk.svg');
-
-
-function About2(props) {
-  return (
-    <div className="about__content">
-      <Helmet>
-        <title>Editor da Escola de Inventor About</title>
-      </Helmet>
-      <div className="about__content-column">
-        <InlineSVG className="about__logo" src={squareLogoUrl} alt="p5js Square Logo" />
-        {/* Video button to hello p5 video page */}
-        {/* <p className="about__play-video">
-          <a
-            href="http://hello.p5js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__play-video-button" src={playUrl} alt="Play Hello Video" />
-          Play hello! video</a>
-        </p>  */}
-      </div>
-      <div className="about__content-column">
-        <h3 className="about__content-column-title">Este Site</h3>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          SE VOCE ESTA VENDO ESSA FRASE É PQ CHAMAR A PAGINA FUNCIONOU
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          O que é Este Site?
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          O que é Maker?
-          </a>
-        </p>
-      </div>
-      <div className="about__content-column">
-        <h3 className="about__content-column-title">Recursos</h3>
-        <p className="about__content-column-list">
-          <a
-            href="https://tableless.github.io/iniciantes/manual/js/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          Exemplos
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://escoladeinventor.com.br/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          Aprenda
-          </a>
-        </p>
-        <p className="about__content-column-list">
-          <a
-            href="https://discourse.processing.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InlineSVG className="about__content-column-asterisk" src={asteriskUrl} alt="p5 asterisk" />
-          Comandos
-          </a>
-        </p>
-      </div>
-      <div className="about__footer">
-        <p className="about__footer-list">
-          <a
-            href="https://escoladeinventor.com.br/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Escola de Inventor
-          </a>
-        </p>
-        <p className="about__footer-list">
-          <a
-            href="https://www.facebook.com/escoladeinventor/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Facebook
-          </a>
-        </p>
-        <p className="about__footer-list">
-          <a
-            href="https://www.instagram.com/escoladeinventor/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Instagram
-          </a>
-        </p>
-      </div>
-    </div>
-  );
-}
-
 export default About2;
+import React from 'react';
+import ReactDOM from 'react-dom';
+var listOfImages =[];
+
+class About2 extends React.Component{
+    importAll(r) {
+        return r.keys().map(r);
+    }
+    componentWillMount() {
+        listOfImages = this.importAll(require.context('./images/', false, /\.(png|jpe?g|svg)$/));
+    }
+    render(){
+        return(
+          <div>
+              {
+                    listOfImages.map(
+                      (image, index) =>    <img key={index} src={image} alt="info"></img>
+                    )
+              }
+          </div>
+        )
+    }
+}
