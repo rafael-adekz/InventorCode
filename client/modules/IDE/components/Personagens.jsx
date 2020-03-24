bf574dd126391413193e1468228c97e8ad524374
leonardof131 <leonardof13@gmail.com>
2020-03-24 13:46:43 -0300

    testando lançamento dos outros menus biblioteca

--- client/modules/IDE/components/Personagens.jsx
+++ client/modules/IDE/components/Personagens.jsx
@@ -6,7 +6,7 @@ const elementosUrl = require('../../../images/elementosicon.svg');
 // const playUrl = require('../../../images/play.svg');
 const asteriskUrl = require('../../../images/p5-asterisk.svg');
 
-const images = [('http://placehold.it/100x100/45AF99'), ('http://placehold.it/200x200/45AF99'),('http://placehold.it/150x150/45AF99'),('http://placehold.it/300x300/45AF99'),('http://placehold.it/100x100/45AF99'), ('http://placehold.it/200x200/45AF99'),('http://placehold.it/150x150/45AF99'),('http://placehold.it/300x300/45AF99'),];
+const images = [('./images/t3.png'), ('http://placehold.it/200x200/45AF99'),('http://placehold.it/150x150/45AF99'),('http://placehold.it/300x300/45AF99'),('http://placehold.it/100x100/45AF99'), ('http://placehold.it/200x200/45AF99'),('http://placehold.it/150x150/45AF99'),('http://placehold.it/300x300/45AF99'),];
 
 
 function Personagens(props) {
