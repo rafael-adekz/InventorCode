daf2d07a6b9e0ceec54c71bf8c47f69e0d6e9a6d
leonardof131 <leonardof13@gmail.com>
2020-03-26 11:07:15 -0300

    apenas um teste

--- client/modules/IDE/reducers/files.js
+++ client/modules/IDE/reducers/files.js
@@ -31,6 +31,7 @@ const defaultHTML =
     <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8" />
   </head>
+  
   <body>
     <div id="conteiner">
     <img src="https://codigosinventor.s3-sa-east-1.amazonaws.com/t7.png" class="img"> 
