1d381bbe3a170ed2416abdcc1e04c319c4f2a271
leonardof131 <leonardof13@gmail.com>
2020-04-15 15:57:02 -0300

    subindo atualizacao filesjs

--- client/modules/IDE/reducers/files.js
+++ client/modules/IDE/reducers/files.js
@@ -12,7 +12,7 @@ const defaultHTML =
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.sound.min.js"></script>
     <script src="https://rawcdn.githack.com/molleindustria/p5.play/42cd19c39f6f508e4a73c20eaaeb490b97022840/lib/p5.play.js"></script>
-    <script src="${'./esyEditor.js'}"></script>
+    <script src="https://codigosinventor.s3-sa-east-1.amazonaws.com/easyEditor.js"></script>
     <script src="https://codigosinventor.s3-sa-east-1.amazonaws.com/fundo_VS_3_Com+Sprites.js"></script>
     <script src="https://codigosinventor.s3-sa-east-1.amazonaws.com/sprites_vs.7.js"></script>
     <script src="https://rawcdn.githack.com/yining1023/p5PlayGround/cad807762776d1f29820bc9f35f36e98cc18d934/js/jquery.js" type="text/javascript"> </script>
