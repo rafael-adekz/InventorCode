93c212e00f889f0782281512f3486d907f48f3c6
rafael-adekz <45203454+rafael-adekz@users.noreply.github.com>
2020-06-24 15:44:28 -0300

    Listagem dos arquivos das pastas de audio, sprites e fundos
    Listagem dos arquivos das pastas de audio, sprites e fundos

--- client/modules/IDE/components/Fundos.jsx
+++ client/modules/IDE/components/Fundos.jsx
@@ -3,7 +3,6 @@ import InlineSVG from 'react-inlinesvg';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router';
 
-
 const elementosUrl = require('../../../images/elementosicon.svg');
 // const playUrl = require('../../../images/play.svg');
 const asteriskUrl = require('../../../images/p5-asterisk.svg');
@@ -19,17 +18,99 @@ const copyUrl = require('../../../images/new/copy.svg');
 const bugUrl = require('../../../images/new/bug.svg');
 
 
-const images = [(asteriskUrl), (elementosUrl),];
+//const images = [(asteriskUrl), (elementosUrl),];
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const getFileName = (name) => {
+  let split = name.split(".");
+  split.pop();
+  return split.join(".");
+}
+
+const images = importAll(require.context('./background', false, /\.(png|jpe?g|svg)$/));
+
+let pagina = 0;
+let por_pagina = 16;
+let total_paginas = Math.ceil(Object.keys(images) / por_pagina);
+
+const proximaPagina = (p) => {
+  console.log('proximaPagina',p);
+  pagina = p+1;
+}
+
+const paginaAnterior = (p) => {
+  pagina = p-1;
+}
+
+const copyToClipboard = (text) => {
+  const el = document.createElement('textarea');
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
 
 function Fundos(props) {
+  
+  let imageList = () => {
+    let retorno = <div>Nenhuma imagem disponível</div>
+
+    if(Object.keys(images).length > 0){
+      let list = [];
+      for(let i in images){
+        list.push(
+          <div className="box" key={"image_"+i} onClick={() => {copyToClipboard(getFileName(i))}} style={{cursor:'pointer'}}>
+            <img src={images[i]} alt={i} style={{width:100,height:100}} />
+            <p>{getFileName(i)}</p>
+          </div>
+        )
+      }
+
+      let pp = por_pagina/2;
+      let p1 = list.slice(pp*pagina,pp*(pagina+1));
+      let p2 = list.slice(pp*(pagina+1),pp*(pagina+2));
+      console.log('p2',p2);
+
+      retorno = <div className="book-content">
+      <div className="w50">
+        <div className="search d-flex a-center">
+          <input type="text" placeholder="Buscar" />
+          <button className="btn-search">
+            <InlineSVG src={searchUrl} alt="" />
+          </button>
+        </div>
+        <div className="list">
+          {p1}
+        </div>
+      </div>
+
+      <div className="w50 right">
+        <div className="buttons d-flex jc-end">
+          <button className="btn" onClick={() => {paginaAnterior(pagina)}}>Anterior</button>
+          <button className="btn" onClick={() => {proximaPagina(pagina)}}>Proximo</button>
+        </div>
+        <div className="list">
+          {p2}
+        </div>
+      </div>
+    </div> 
+    }
+    return retorno;
+  }
+
   return (
     <div className="bg-black">
         
     <div className="book">
       <div className="book-tabs">
         <div className="tab">
-        <Link to="/personagens"><span>Sprite</span></Link> 
+          <Link to="/personagens"><span>Sprite</span></Link> 
              
           <InlineSVG src={tlogoUrl} alt="" />
         </div>
@@ -45,100 +126,18 @@ function Fundos(props) {
         <Link to="/personagens"><span>Sons</span></Link>
           <InlineSVG src={audioUrl} alt="" />
         </div>
-        <div className="tabdn">
-          <span>Sons</span>
-          <InlineSVG src={musicaUrl} alt="" />
+        <div className="tab">
+        <Link to="/sons"><span>Sons</span></Link>
+          <InlineSVG src={audioUrl} alt="" />
         </div>
         <div className="tabdn">
           <span>Comandos</span>
           <InlineSVG src={codeUrl} alt="" />
         </div>
       </div>  
-      <div className="book-content">
-        <div className="w50">
-        {/**  <div className="search d-flex a-center">
-            <input type="text" placeholder="Buscar" />
-            <button className="btn-search">
-              <InlineSVG src={searchUrl} alt="" />
-            </button>
-          </div> */}
-          <div className="list">
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>    
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-          </div>
-        </div>
 
-        <div className="w50 right">
-        {/**   <div className="buttons d-flex jc-end">
-            <button className="btn">Anterior</button>
-            <button className="btn">Proximo</button>
-          </div>*/}
-          <div className="list">
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-            <div className="box">
-              <img src={'https://placehold.it/100'} alt="Logo" />
-              <p>Sprite n+</p>
-            </div>
-          </div>
-        </div>
-      </div>        
+      {imageList()}
+       
     </div>
 
   </div>
